8437d98b38d07098f11a9dfddfe04262ab316d3f
ahmed <ahmedbelm51@gmail.com>
2025-07-30 14:06:18 -0400

    Get random word

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { WordService } from '../services/word';
 
 @Component({
   selector: 'app-game',
   imports: [],
   templateUrl: './game.html',
-  styleUrl: './game.css'
+  styleUrls: ['./game.css']
 })
 export class Game {
+
+  randomWord: string= '';
+
+  constructor(private wordService: WordService) {
+    this.wordService.getRandomWord().subscribe(word => {
+      this.randomWord = word;
+    });
+  }
+  validateWord(word: string): void {
+    this.wordService.validateWord(word).subscribe(isValid => {
+      if (isValid) {
+        console.log(`${word} is a valid word.`);
+      } else {
+        console.log(`${word} is not a valid word.`);
+      }
+    });
+  }
 
 }
--- src/app/services/word.ts
+++ src/app/services/word.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WordService {
-  private apiUrl = 'https://api.frontendexpert.io/api/fe/wordle-words'; 
+  private apiUrlWordle = 'https://api.frontendexpert.io/api/fe/wordle-words'; 
+  private apiUrlDictionary = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 
   constructor(private http: HttpClient) {}
 
   getWordList(): Observable<string[]> {
-    return this.http.get<string[]>(this.apiUrl, { headers: {Accept: 'application/json'} });
+    return this.http.get<string[]>(this.apiUrlWordle , { headers: {Accept: 'application/json'} });
   }
   getRandomWord(): Observable<string> {
     return this.getWordList().pipe(
       map(words => words[Math.floor(Math.random() * words.length)].toUpperCase())
+    );
+  }
+  validateWord(word: string): Observable<boolean> {
+    return this.http.get<any[]>(`${this.apiUrlDictionary}${word}`).pipe(
+      map(() => true),
+      catchError(() => of(false))
     );
   }
 }
